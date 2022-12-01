2a93c83c1c517404741eed60d16bcdcdf6ec9f0a
Jake Browning <jacobbrowning90@gmail.com>
2022-12-01 16:03:16 +0000

    Test commit with altered directory setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { drawHome } from '../src/homeScreen.js';
 import { drawGreeting } from '../src/drawGreeting';
+import './reset.css';
+import './style.css';
 
 console.log("Index.js flowing!");
 drawHome();
